f83a586cafd4722a3911dbeb4fcc7dbeb6a59450
571530 <Truls.Martinsen13@gmail.com>
2018-09-07 14:09:52 +0200

    init

---
+++ Kapittel12/Oppgaver.py
@@ -0,0 +1,68 @@
+def caesar(txt, rotate) :
+	crypt = ""
+	for char in txt:
+		if ord(char) >= ord('a') and ord(char) <= ord('z'):
+			crypt = crypt + chr((ord(char) - ord('a') + rotate) % 26 + ord('a'))
+	return crypt
+
+def frequencyOfString(file):
+	frekvenser = [0]*26
+
+	antall = 0
+	with open(file) as f:
+		for line in f:
+			for ch in line:
+				c = ch.lower()
+				if ord(c) >= ord('a') and ord(c) <= ord('z'):
+					frekvenser[ord(c)-ord('a')] += 1
+					antall += 1
+
+	return [x / antall for x in frekvenser]
+
+def compareFrequency(comp, ref):
+	sum = 0;
+	for i in range(0, 26):
+		sum += ((comp[i] - ref[i])**2) / ref[i]
+	return sum
+
+def oppgave1():
+	for i in range(0, 26):
+		print(caesar("xcy", i))
+			
+
+def oppgave2():
+	for i in range(0, 26):
+		txt = caesar("xcy", i)
+		if txt.count('e') > 0:
+			print(txt)	
+
+def oppgave4():
+	comp = frequencyOfString("romeoogjuliet.txt")
+	ref = frequencyOfString("macbeth.txt")
+	print(compareFrequency(comp, ref))
+
+def oppgave5():
+	for i in range(0, 26):
+		txt = caesar("uibpmuibqkaqauwzmncvbpivxzwoziuuqvo", i)
+		if txt.count('e') > 1:
+			print(txt)	
+
+def test():
+	txt = "uibpmuibqkaqauwzmncvbpivxzwoziuuqvo"
+
+	ref = frequencyOfString("macbeth.txt")
+
+	minX = compareFrequency(frequencyOfString("kode"), ref)
+	minTxt = "uibpmuibqkaqauwzmncvbpivxzwoziuuqvo"
+
+	for i in range(1, 26):
+		encrypt = caesar(txt, i)
+		freq = frequencyOfString("kode")
+		x = compareFrequency(freq, ref);
+		if (x < minX):
+			minX = x
+			minTxt = encrypt
+
+	print(minTxt)
+
+test()
